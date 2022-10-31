b34056d3dd8ccb1f28ddcf3d1670c86fe66fa476
Jack Yang <z3079335@ad.unsw.edu.au>
2022-10-31 15:02:15 +1100

    structural variation visualisation

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from ase.geometry.analysis import Analysis
+import numpy as np
+
 termination_types = {'100': ['AO', 'BO2'], '110': ['O2', 'ABO'], '111': ['AO3', 'B']}
 
 charge_state_A_site = {0: 1, 1: 1, 2: 2, 3: 1}
@@ -9,6 +12,9 @@ A_site_list = [['Li', 'Na', 'K', 'Rb', 'Cs'], ['Li', 'Na', 'K', 'Rb', 'Cs'], ['M
 B_site_list = [['Pb', 'Sn', 'Ge'], ['V', 'Ta', 'Nb'], ['Ti', 'Zr'], ['V', 'Ta', 'Nb']]
 C_site_list = [['F', 'Cl', 'Br', 'I'], ['F', 'Cl', 'Br', 'I'], ['O', 'S', 'Se', 'Te'], ['O', 'S', 'Se', 'Te']]
 
+charge_state_A_site = {0: 1, 1: 1, 2: 2, 3: 1}
+charge_state_B_site = {0: 2, 1: 2, 2: 4, 3: 5}
+charge_state_C_site = {0: -1, 1: -1, 2: -2, 3: -2}
 
 def load_all_uids():
     import os,glob
@@ -30,3 +36,39 @@ def load_all_uids():
                     if this_uid not in all_uids:
                         all_uids.append(this_uid)
     return all_uids
+
+#some helper functions to get the BO-bond vectors in different structures
+def BO_bond_vectors_in_bulk_perovskite(db, a, b, c):
+    system_name = a + b + c
+    uid = system_name + '3_pm3m'
+    row = db.get(selection=[('uid', '=', uid)])
+    atoms = row.toatoms()
+    analyzer = Analysis(atoms)
+    all_vectors = []
+    for bonds in analyzer.get_bonds(b, c)[0]:
+        v = atoms.get_distances(bonds[0], [bonds[1]], mic=True, vector=True)[0]
+        #v = np.array(*atoms.get_distances(bonds[0], [bonds[1]], mic=True, vector=True)[0])
+        #print(v)
+        #v = cVector3D(*atoms.get_distances(bonds[0], [bonds[1]], mic=True, vector=True)[0])
+        #v = cVector3D(*v)
+        all_vectors.append(v)
+    return all_vectors
+
+
+def BO_bond_vectors_in_twod_perovskites(db, a, b, c, orientation, termination, thickness):
+    system_name = a + b + c
+    uid = system_name + '3_' + str(orientation) + "_" + str(termination) + "_" + str(thickness)
+    #print(uid)
+    try:
+        row = db.get(selection=[('uid', '=', uid)])
+    except:
+        return None
+    atoms = row.toatoms()
+    analyzer = Analysis(atoms)
+    all_vectors = []
+    for bonds in analyzer.get_bonds(b, c)[0]:
+        v = atoms.get_distances(bonds[0], [bonds[1]], mic=True, vector=True)[0]
+        #v = cVector3D(*atoms.get_distances(bonds[0], [bonds[1]], mic=True, vector=True)[0])
+        #v = cVector3D(*v)
+        all_vectors.append(v)
+    return all_vectors
